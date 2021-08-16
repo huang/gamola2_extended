f1252e3846849a13b8b76f04f77e37d5ad0b4cb9
Jiabin Huang <j.b.huang@hotmail.com>
2021-08-16 15:49:37 +0200

    Add files via upload

---
+++ construct_Results.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#rm -rf Results
+mkdir Results
+cd Results
+mkdir IG_Blast_results
+mkdir Sequin_submission
+mkdir Annotation_transfer
+mkdir Session_comparison
+mkdir tRNAscan_results
+mkdir non_coding_RNA_results
+mkdir rRNA_results
+mkdir vector_results
+mkdir CRISPR_results
+mkdir Blast_results_DEL
+mkdir tmhmm_results
+mkdir TransTerm_results
+mkdir Genbank_annotation
+mkdir gene_models
+mkdir PFam_results
+mkdir TIGRfam_results
+mkdir SignalP_results
+mkdir Blast_results
+mkdir COG_results
