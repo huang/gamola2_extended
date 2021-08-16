deccc86ef29d9f5db0fd9ba7b92a700ffd6860f8
Jiabin Huang <j.b.huang@hotmail.com>
2021-08-16 15:49:14 +0200

    Add files via upload

---
+++ clean_Results.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -rf Results/Blast_results/*
+rm -rf Results/COG_results/*
+rm -rf Results/PFam_results/*
+rm -rf Results/TIGRfam_results/*
+rm -rf Results/SignalP_results/*
+rm -rf Results/non_coding_RNA_results/*
+rm -rf Results/rRNA_results/*
+rm -rf Results/tmhmm_results/*
+rm -rf Results/TransTerm_results/*
+rm -rf Results/vector_results/*
+rm -rf Results/CRISPR_results/*
+rm -rf Results/Genbank_annotation/*
